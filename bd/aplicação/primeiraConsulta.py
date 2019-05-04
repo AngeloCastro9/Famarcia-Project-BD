6cb76ecb4582af8042d4dbf510d715a61cb5e426
angelocastro9 <castromatheus060@gmail.com>
2019-05-04 15:17:15 -0300

    correção na conexão com o banco

--- bd/aplicação/primeiraConsulta.py
+++ bd/aplicação/primeiraConsulta.py
@@ -1,6 +1,6 @@
 import mysql.connector
 
-cnx = mysql.connector.connect(user='root', password='An323131',
+cnx = mysql.connector.connect(user='root', password='root',
                               host='127.0.0.1',
                               database='sicapharma')
 
